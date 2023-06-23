df62c0f6785d502290d590031b877515abb0af71
Abid <ghulamabidhassan@gmail.com>
2023-06-23 20:20:08 +0400

    coding bootcamp

--- 6-Coding-Bootcamp/src/App.jsx
+++ 6-Coding-Bootcamp/src/App.jsx
@@ -31,11 +31,7 @@ function App() {
     <div className="main">
       <div className="container">
         <section className="section-one">
-          <img
-            src="../public/images/pattern-bg.svg"
-            alt=""
-            className="bg-pattern"
-          />
+          <img src="../public/pattern-bg.svg" alt="" className="bg-pattern" />
 
           <div className="box">
             <img src={data[index].img} alt="" className="avatar" />
@@ -46,11 +42,7 @@ function App() {
           </div>
         </section>
         <section className="section-two">
-          <img
-            src="../public/images/pattern-quotes.svg"
-            alt=""
-            className="quotes"
-          />
+          <img src="../public/pattern-quotes.svg" alt="" className="quotes" />
           <div className="content">
             <p className="desc">
               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
